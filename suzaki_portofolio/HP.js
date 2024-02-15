863484b1300c4907c7652ddc2089e3dc345865e1
Suzaki-Aru <s.aruru.ks4@gmail.com>
2024-02-15 11:08:20 +0900

    HTML,JSの編集

--- suzaki_portofolio/HP.js
+++ suzaki_portofolio/HP.js
@@ -1,1 +1,26 @@
-// 須崎
+let text1 = document.getElementById("mm")
+text1.addEventListener("mouseover", function() {
+    this.style.color = "gray";
+});
+
+text1.addEventListener("mouseleave", function() {
+    this.style.color = "black";
+});
+
+document.getElementById("plane").addEventListener("click", function() {
+    var plane = this;
+    var width = document.documentElement.clientWidth; // ビューポートの幅を取得
+    plane.style.transition = "left 2s ease-in-out"; // アニメーション効果を設定
+    plane.style.left = width + "px"; // 飛行機を画面外まで移動させる
+  });
+
+  document.getElementById("kitetsu").addEventListener("click", function() {
+    document.getElementById("kitetsu").style.left = "200px";
+  })
+
+  document.getElementById("kitetsu").addEventListener("click", function() {
+    var plane = this;
+    var width = document.documentElement.clientWidth; // ビューポートの幅を取得
+    plane.style.transition = "left 2s ease-in-out"; // アニメーション効果を設定
+    plane.style.left = width + "px"; // 飛行機を画面外まで移動させる
+  });
